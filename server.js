8fbff18c96574edebd769d5be1ddb4ce648b397b
MajemiteJames <majemiteokoro@gmail.com>
2020-07-18 06:36:51 +0100

    implemented non existing endpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
 
 const app = express();
@@ -8,6 +9,7 @@ connectDB();
 app.get("/", (req, res) => res.send("API Running"));
 
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 app.use("/api/users", require("./routes/api/user"));
 app.use("/api/search", require("./routes/api/search"));
@@ -15,3 +17,10 @@ app.use("/api/search", require("./routes/api/search"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.all("*", (req, res) =>
+  res.status(404).json({
+    status: 404,
+    error: "Endpoint does not exist",
+  })
+);
